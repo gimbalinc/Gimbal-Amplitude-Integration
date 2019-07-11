2ba7ee928e6532faa85113c7aa1bed50c52c27fb
Victor Nunez <nunez.ve@gmail.com>
2019-07-11 02:03:35 -0400

    adds project

---
+++ hello-gimbal-swift/AppDelegate.swift
@@ -0,0 +1,33 @@
+import UIKit
+import UserNotifications
+
+@available(iOS 10.0, *)
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, GMBLCommunicationManagerDelegate {
+    var notificationCenter : UNUserNotificationCenter!
+    
+    var window: UIWindow?
+    
+    @objc(application:didFinishLaunchingWithOptions:) func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil))
+        
+        notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.delegate = self as UNUserNotificationCenterDelegate
+    
+        return true
+    }
+    
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                         willPresent notification: UNNotification,
+                                         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void){
+        completionHandler(.alert)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        GMBLCommunicationManager.communication(for: response)
+        completionHandler()
+    }
+    
+}
+
